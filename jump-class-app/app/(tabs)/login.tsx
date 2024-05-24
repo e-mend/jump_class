05601156224e34f9ba275291850680a9cadd79e4
Gabriel Paiva <paiva.gabriel911@gmail.com>
2024-05-24 19:28:27 -0300

    Adding UI

--- jump-class-app/app/(tabs)/login.tsx
+++ jump-class-app/app/(tabs)/login.tsx
@@ -7,15 +7,19 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
-export default function TabTwoScreen() {
+export default function TabThreeScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">
+          Login
+        <Ionicons size={20} name="code-slash" />
+      </ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
+      <ThemedText>This app includes example code to help you get started.
+      </ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
           This app has two screens:{' '}
